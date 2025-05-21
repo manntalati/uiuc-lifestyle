0ee23291111e826cce1eb0cee0c5eece577908e5
Mann Talati <mann.talati@gmail.com>
2025-05-20 21:23:14 -0500

    changes

---
+++ frontend/app/(tabs)/login.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function LoginScreen() {
+    return (
+        <ThemedView>
+        <ThemedText>Login</ThemedText>
+        </ThemedView>
+    );
+}
