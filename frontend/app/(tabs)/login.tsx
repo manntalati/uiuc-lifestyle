d73f4944a050f001fa2f4e33e42b733d9b7f7d23
Mann Talati <mann.talati@gmail.com>
2025-05-22 18:42:58 -0500

    frontend login work

--- frontend/app/(tabs)/login.tsx
+++ frontend/app/(tabs)/login.tsx
@@ -3,6 +3,9 @@ import { ThemedView } from '@/components/ThemedView';
 import { Platform, View, StyleSheet, FlatList, Dimensions } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { useState } from 'react';
+import { SearchBar } from 'react-native-screens';
+import { Button } from '@react-navigation/elements';
+import { FaAlignCenter } from 'react-icons/fa';
 
 
 export default function LoginScreen() {
@@ -14,7 +17,7 @@ export default function LoginScreen() {
     const login = async () => {
         try {
             setIsLogging(true);
-            const res = await fetch("https://ipaddress:8082/login");
+            const res = await fetch("http://10.0.0.107:8082/login");
         } catch (error) {
             console.error(error);
         }
@@ -24,7 +27,7 @@ export default function LoginScreen() {
     const register = async () => {
         try {
             setIsLogging(true);
-            const res = await fetch("https://ipaddress:8082/register");
+            const res = await fetch("http://10.0.0.107:8082/register");
         } catch (error) {
             console.error(error);
         }
@@ -37,28 +40,65 @@ export default function LoginScreen() {
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
             headerImage={<View style={{height: 0}} />}>
-            {/*<FormData>*/}
+            <View style={styles.screen}>
+                <ThemedView style={[styles.container, {width: width * 0.4, height: height * 0.22}, {borderRadius: 10}]}>
+                    <View style={styles.form}>
+                        <form>
+                            <ThemedText style={{color: "black"}}>Username: </ThemedText>
+                            <input name="username"/>
+                            <ThemedText style={{color: "black"}}>Password: </ThemedText>
+                            <input name="password" type="password" />
+                        </form>
+                    </View>
 
-            {/*</FormData>*/}
+                    <View style={styles.buttonRow}>
+                        <Button
+                            onPress={login}
+                            style={[{backgroundColor: "#f0f0f0"}, {marginRight: 20, marginTop: 45}, {borderRadius: 0}]}>Login</Button>
+                        <Button
+                            onPress={register}
+                            style={[{backgroundColor: "#f0f0f0"}, {marginRight: 20, marginTop: 45}, {borderRadius: 0}]}>Register</Button>
+                    </View>
+                </ThemedView>
+            </View>
         </ParallaxScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    form: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
     titleContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: 8,
     },
     stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 10,
+    }
 })
